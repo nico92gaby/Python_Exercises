f246ab784ed90f3315a9395835eec4a16e09f318
Nicolás Suarez <nico92gaby@gmail.com>
2023-07-04 08:14:38 -0300

    Se sube 5.Exercise2 para prácticar desde el trabajo

---
+++ 5.Exercises2.py
@@ -0,0 +1,37 @@
+"""
+Ejercicio 1: La string mas larga:
+Crear una función que reciba una lista de string como entrada y te diga cual es la mas larga de todas
+
+Ej: srtring_mas_larga("hola", "como", "estas")
+>estas
+
+Ejercicio 2_ Sumando la lista
+Crear una funcion que sume una lista de numeros, no se vale usar la funcion sum
+Ej: suma ([1, 2, 3, 4, 5])
+>15
+
+Ejercicio 3: Par o impar
+Crear una funcion que te de True como resultado si el numero pasado com oargumento es impar.
+Ej: es_impar(3)
+>True
+Es_impar(24)
+>False
+
+Ejercicio 4: Pregunta algo
+Cra una funcion que pregunte al usuario si esta seguro o no, y devuelva los valores "True" o "False" dependiendo
+de si el usuario esta seguro o no.
+
+Ejercicio 5: A mayus
+Crear una funcion que convierta toda una string en mayusculas, no vale usar el metodo upper()
+
+Ejercicio 6: Adivina el número
+Crear una funcion que reciba como argumento del 1 al 100 a adivinar y que le pregunte al usuario que adivine el numero.
+el codigo se ejecutará hasta que el usuario adivine.
+
+Ejercicio 7: Lista de la compra
+Crear una funcion que dada una lista de la compra definida fuera de la función,
+permita al usuario añadir un nuevo item asegurandose que no exista anteriormente en la lista.
+
+
+"""
+
